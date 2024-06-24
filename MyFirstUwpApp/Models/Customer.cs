90569264ee9c2a01b5afb62926854bd2390df49d
Roman Portianko <rportyanko10@gmail.com>
2024-06-24 12:31:43 +0300

    Created models for the app

---
+++ MyFirstUwpApp/Models/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyFirstUwpApp.Models
+{
+    internal class Customer : ObservableObject
+    {
+        private string _firstName;
+        private string _lastName;
+
+        [JsonPropertyName("firstName")]
+        public string FirstName 
+        {
+            get => _firstName; 
+            set 
+            { 
+                if (_firstName != value) 
+                { 
+                    _firstName = value; 
+                    OnPropertyChanged(); 
+                } 
+            }
+        }
+
+        [JsonPropertyName("lastName")]
+        public string LastName
+        { 
+            get => _lastName;
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
