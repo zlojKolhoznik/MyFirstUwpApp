a354457af422821ef74bc485924d7aca2014bfd5
Roman Portianko <rportyanko10@gmail.com>
2024-06-24 13:55:17 +0300

    Added messaging services to enable communication between user and ViewModel

---
+++ MyFirstUwpApp/Services/MessageService/IMessageService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstUwpApp.Services.MessageService
+{
+    public interface IMessageService
+    {
+        Task ShowPlainMessageAsync(string message);
+        Task ShowErrorMessageAsync(string message);
+        Task<MessageResponse> ShowPromptAsync(string message, PromptType type);
+    }
+
+    public enum MessageResponse
+    {
+        Yes,
+        No,
+        Ok,
+        Cancel
+    }
+
+    public enum PromptType
+    {
+        YesNo,
+        YesNoCancel,
+        OkCancel
+    }
+}
